3cc478480251258a22685133800b5982acb83b35
Naofumi Kagami <naofumi@mac.com>
2014-02-23 09:57:36 +0900

    Added parameter sanitization code

--- view_helpers.php
+++ view_helpers.php
@@ -8,8 +8,18 @@ function odd_even($values = array("even", "odd")){
   $odd_even_cycle++;
   return $result;
 }
+
 function odd_even_reset(){
   global $odd_even_cycle;
   $odd_even_cycle = 0;
 }
 
+// Santize and decode a single param value
+function sanitize_param($param) {
+  return htmlspecialchars(mb_convert_encoding($param, 'UTF-8', array('SJIS', 'UTF-8', 'EUC-JP')));
+}
+
+function dasherize($string) {
+  $lower = strtolower($string);
+  return preg_replace("/(\W)/", "_", $lower);
+}
