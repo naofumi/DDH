5dabeac90eb789655f0470291b2108b6752366a2
Naofumi Kagami <naofumi@mac.com>
2014-02-25 13:22:06 +0900

    Allow rowspans to be calculated for non-native fields (fields that are not set in $source_parameters).

--- data_row.php
+++ data_row.php
@@ -42,16 +42,24 @@ class DataRowBase {
 	// with the rowspan generated from $row[$field."_rowspan"] value.
 	// Additional attributes such as `class` and `style` can be
 	// set using the $attributes argument.
-	function td($field, $attributes = array()) {
+	//
+	// If the $field is not a native field in the $source_parameters,
+	// then it won't have a $row[$field."_rowspan"] value. In these
+	// cases, provide a native field in the $field_for_rowspan argument. This field will
+	// be used to get the $row[$field."_rowspan"] value.
+	function td($field, $attributes = array(), $field_for_rowspan = null) {
+		if (!$field_for_rowspan) {
+			$field_for_rowspan = $field;
+		}
 	  $result = "";
 	  $attribute_string = "";
 	  foreach($attributes as $name => $value) {
 	    $attribute_string = $attribute_string." $name=\"$value\"";
 	  }
-	  if ($this->get($field."_rowspan") && $this->get($field."_rowspan") > 1) {
-	    $attribute_string = $attribute_string." rowspan=".$this->get($field."_rowspan");
+	  if ($this->get($field_for_rowspan."_rowspan") && $this->get($field_for_rowspan."_rowspan") > 1) {
+	    $attribute_string = $attribute_string." rowspan=".$this->get($field_for_rowspan."_rowspan");
 	  }
-	  if (!$this->get($field."_rowspan") || $this->get($field."_rowspan") != -1) {
+	  if (!$this->get($field_for_rowspan."_rowspan") || $this->get($field_for_rowspan."_rowspan") != -1) {
 	    $result = "<td$attribute_string>";
 	    $result = $result.$this->get($field);
 	    $result = $result."</td>\n";      
