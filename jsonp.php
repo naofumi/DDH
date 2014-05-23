d2c62b0e1d6f28cfe86141d064a5b9a8fdefc71b
Naofumi Kagami <naofumi@mac.com>
2014-05-23 10:42:54 +0900

    Suppress "_SESSION not defined" warnings in the log

--- jsonp.php
+++ jsonp.php
@@ -314,7 +314,7 @@ function basic_auth(){
 function log_request() {
 	error_log($_SERVER['REQUEST_METHOD']." ".$_SERVER['REQUEST_URI']." Accept:".
 	          $_SERVER['HTTP_ACCEPT']." IP:".$_SERVER['REMOTE_ADDR']." Request:".var_export($_REQUEST, true).
-	          " Session:".var_export($_SESSION, true) );
+	          " Session:".var_export((defined('_SESSION') ? $_SESSION : null), true) );
 }
 
 ///////////////////////////////////////////////////////////
